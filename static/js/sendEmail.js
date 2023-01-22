efb7ce05d49d854981f72c7751c58a76890f9e81
Emma Scott <emmajane1276@gmail.com>
2023-01-22 01:09:57 +0000

    email functionality created

--- static/js/sendEmail.js
+++ static/js/sendEmail.js
@@ -1,1 +1,17 @@
-console.log("Hello Emma")
+/*-------- sendmail() function --------*/
+function sendMail(contactForm) {
+    emailjs.send("gmail", "happycamper", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "contactsummary": contactForm.contactsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("YES SUCCESS", response);
+        },
+        function(error) {
+            console.log("OH NO!!!!", error);
+        }
+    );
+    return false;
+}
