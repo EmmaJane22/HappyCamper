3d751bd7c029b1a921480f589f26ce82a3c0cdb6
Emma Scott <emmajane1276@gmail.com>
2023-01-22 12:22:46 +0000

    email functionality added

--- static/js/sendEmail.js
+++ static/js/sendEmail.js
@@ -1,15 +1,19 @@
 /*-------- sendmail() function --------*/
 function sendMail(contactForm) {
-    emailjs.send("gmail", "happycamper", {
+    emailjs.send("service_c23rfuq", "happycamper", {
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
         "contactsummary": contactForm.contactsummary.value
     })
     .then(
         function(response) {
+            alert("Email sent successfully!");
             console.log("YES SUCCESS", response);
+            window.location.href = "/";
+            return true;
         },
         function(error) {
+            alert("Email failed-please try again");
             console.log("OH NO!!!!", error);
         }
     );
